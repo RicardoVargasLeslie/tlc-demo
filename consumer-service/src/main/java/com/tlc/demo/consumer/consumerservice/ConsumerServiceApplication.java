3ad725602e0a2c957eaaac3fb517b7f741b69aa5
rvarles <25244791+RicardoVargasLeslie@users.noreply.github.com>
2020-12-20 21:54:16 +0000

    basic build

---
+++ consumer-service/src/main/java/com/tlc/demo/consumer/consumerservice/ConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tlc.demo.consumer.consumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerServiceApplication.class, args);
+	}
+
+}
