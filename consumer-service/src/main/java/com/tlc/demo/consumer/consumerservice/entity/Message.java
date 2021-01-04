7aa9fc6daa21203c42f3fd27fc3e58060ec0e1c6
Ricardo Vargas <r.vargas.leslie@accenture.com>
2021-01-04 10:54:15 +0100

    test with tag

--- consumer-service/src/main/java/com/tlc/demo/consumer/consumerservice/entity/Message.java
+++ consumer-service/src/main/java/com/tlc/demo/consumer/consumerservice/entity/Message.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 @Document(collection = "message")
 @NoArgsConstructor
 @Data
-public class Message  {
+public class Message implements Serializable {
 
     @Id
     @JsonIgnore
